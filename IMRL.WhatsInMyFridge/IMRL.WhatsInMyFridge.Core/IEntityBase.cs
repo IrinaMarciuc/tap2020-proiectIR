cb362f04a4ed2b5fcacd511ee9e241b5b580c40f
Irina Marciuc <irimarciuc@gmail.com>
2020-05-16 18:07:42 +0300

    Repository pattern

--- IMRL.WhatsInMyFridge/IMRL.WhatsInMyFridge.Core/IEntityBase.cs
+++ IMRL.WhatsInMyFridge/IMRL.WhatsInMyFridge.Core/IEntityBase.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace IMRL.WhatsInMyFridge.Core
 {
-    interface IEntityBase
+    public interface IEntityBase
     {
         public Guid Id { get; set; }
     }
---
+++ IMRL.WhatsInMyFridge/IMRL.WhatsInMyFridge.DataAccess.SqlServer/Repositories/DataRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IMRL.WhatsInMyFridge.DataAccess.Repositories;
+using IMRL.WhatsInMyFridge.Core;
+using System.Linq;
+
+namespace IMRL.WhatsInMyFridge.DataAccess.SqlServer.Repositories
+{
+    public class DataRepository : IDataRepository
+    {
+        void IDataRepository.Delete<TEntity>(TEntity entityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDataRepository.Insert<TEntity>(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        IQueryable<TEntity> IDataRepository.Query<TEntity>()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDataRepository.Update<TEntity>(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
