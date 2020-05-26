311842d2d89325b5fe44675441d6f60a61d1cb86
RLuchian <raluchian@yahoo.com>
2020-05-26 20:05:31 +0300

    Finished Pending Recipe Service

---
+++ src/IMRL.WhatsInMyFridge.Web/Areas/Admin/Models/PendingRecipeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IMRL.WhatsInMyFridge.Core.Recipes.Base;
+
+namespace IMRL.WhatsInMyFridge.Web.Areas.Admin.Models
+{
+    public class PendingRecipeViewModel
+    {
+        public PendingRecipeViewModel()
+        {
+            Results = new List<Recipe>();
+        }
+
+        public List<Recipe> Results { get; internal set; }
+    }
+}
